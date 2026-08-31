84b76f306baba61730781c5c6a6d7683661e25c3
Heaifan <3501337858@qq.com>
2026-08-31 20:01:56 +0800

    fix(xyui): use avalonia templated font property

--- xyui/avalonia/src/XYUI.Avalonia/Controls/XYUI3/_Shared/Styles/XyuiComponentStyles.Batch05.cs
+++ xyui/avalonia/src/XYUI.Avalonia/Controls/XYUI3/_Shared/Styles/XyuiComponentStyles.Batch05.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Media;
 using Avalonia.Styling;
 
@@ -10,7 +11,7 @@ public static partial class XyuiComponentStyles
     static void Batch05(Styles styles)
     {
         var bar = new Style(x => x.OfType<XYCommandBar>().Class("xyui-command-bar")); Brush(bar, Border.BackgroundProperty, "XY.Brush.Surface.Toolbar"); Brush(bar, Border.BorderBrushProperty, "XY.Brush.Border.Color.Default"); bar.Setters.Add(new Setter(Border.HeightProperty, 34d)); bar.Setters.Add(new Setter(Border.BorderThicknessProperty, new Thickness(1))); styles.Add(bar);
-        var command = new Style(x => x.OfType<XYCommandItem>().Class("xyui-command-item")); command.Setters.Add(new Setter(Button.HeightProperty, 28d)); command.Setters.Add(new Setter(Button.PaddingProperty, new Thickness(8, 0))); command.Setters.Add(new Setter(Button.FontSizeProperty, 12.5d)); styles.Add(command);
+        var command = new Style(x => x.OfType<XYCommandItem>().Class("xyui-command-item")); command.Setters.Add(new Setter(Button.HeightProperty, 28d)); command.Setters.Add(new Setter(Button.PaddingProperty, new Thickness(8, 0))); command.Setters.Add(new Setter(TemplatedControl.FontSizeProperty, 12.5d)); styles.Add(command);
         var palette = new Style(x => x.OfType<XYCommandPalette>().Class("xyui-command-palette")); Brush(palette, Border.BackgroundProperty, "XY.Brush.Surface.Overlay"); Brush(palette, Border.BorderBrushProperty, "XY.Brush.Border.Color.Subtle"); palette.Setters.Add(new Setter(Border.WidthProperty, 440d)); palette.Setters.Add(new Setter(Border.PaddingProperty, new Thickness(10))); palette.Setters.Add(new Setter(Border.CornerRadiusProperty, new CornerRadius(5))); styles.Add(palette);
         var result = new Style(x => x.OfType<Button>().Class("xyui-palette-result")); result.Setters.Add(new Setter(Button.HeightProperty, 30d)); result.Setters.Add(new Setter(Button.PaddingProperty, new Thickness(10, 0))); styles.Add(result);
         var back = new Style(x => x.OfType<XYBackForwardNavigation>().Class("xyui-back-forward")); back.Setters.Add(new Setter(Border.HeightProperty, 34d)); back.Setters.Add(new Setter(Border.PaddingProperty, new Thickness(4, 3))); styles.Add(back);
