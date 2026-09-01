a51b4137c832fb352446b8c04cc011524cfcd2f8
Heaifan <3501337858@qq.com>
2026-09-01 21:32:26 +0800

    docs(xyui0): redesign SpacingLayoutView into visual measurement and architectural tier documentation

---
+++ xyui/avalonia/gallery/XYUI.Avalonia.Gallery/Views/SpacingMeasurementVisualSection.axaml.cs
@@ -0,0 +1,8 @@
+using Avalonia.Controls;
+
+namespace XYUI.Avalonia.Gallery.Views;
+
+public partial class SpacingMeasurementVisualSection : UserControl
+{
+    public SpacingMeasurementVisualSection() => InitializeComponent();
+}
