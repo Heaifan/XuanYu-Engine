c6724fa858b514f0120cd883f0f271cb6cc5ed86
Heaifan <3501337858@qq.com>
2026-09-02 00:31:30 +0800

    chore(xyui): add gallery launch helper

---
+++ xyui.bat
@@ -0,0 +1,4 @@
+@echo off
+title XYUI.Avalonia.Gallery
+echo Starting XYUI.Avalonia.Gallery...
+D:\MyApp\sdk-dotnet\dotnet.exe run --project "%~dp0xyui\avalonia\gallery\XYUI.Avalonia.Gallery\XYUI.Avalonia.Gallery.csproj"
