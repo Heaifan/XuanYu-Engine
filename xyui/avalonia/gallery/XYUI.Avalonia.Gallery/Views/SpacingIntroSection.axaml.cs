ce7deb1e73a8482736eedeb7f6f43bfc240cefe4
Heaifan <3501337858@qq.com>
2026-09-01 20:43:13 +0800

    docs(xyui0): restructure SpacingLayoutView into pedagogical 8-part usage documentation

---
+++ xyui/avalonia/gallery/XYUI.Avalonia.Gallery/Views/SpacingIntroSection.axaml.cs
@@ -0,0 +1,8 @@
+using Avalonia.Controls;
+
+namespace XYUI.Avalonia.Gallery.Views;
+
+public partial class SpacingIntroSection : UserControl
+{
+    public SpacingIntroSection() => InitializeComponent();
+}
---
+++ xyui/avalonia/gallery/XYUI.Avalonia.Gallery/Views/SpacingSamplesSection.axaml.cs
@@ -0,0 +1,8 @@
+using Avalonia.Controls;
+
+namespace XYUI.Avalonia.Gallery.Views;
+
+public partial class SpacingSamplesSection : UserControl
+{
+    public SpacingSamplesSection() => InitializeComponent();
+}
